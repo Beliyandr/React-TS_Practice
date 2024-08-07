98a77e48323ba6dbb3b4d277a53681a7214d0eaa
beliyandr <beliyandr@gmail.com>
2024-08-07 16:43:49 +0300

    3

---
+++ UILBI_REACT/RTS/src/App.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { Card, CardVariant } from "./components/Card";
+import { UserList } from "./components/UserList";
+import { IUser } from "./types/types";
+import axios from "axios";
+import List from "./components/List";
+
+export const App = () => {
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  async function fetchUsers() {
+    try {
+      const response = await axios.get<IUser[]>(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      setUsers(response.data);
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  return (
+    <>
+      <Card width="200px" height="200px" variant={CardVariant.primary}>
+        <button> click </button>
+      </Card>
+
+      {/* <UserList users={users} /> */}
+
+      {/* <List
+        items={users}
+        renderItem={(user: IUser) => <UserItem user={user} key={user.id} />}
+      /> */}
+    </>
+  );
+};
