7dde66953cf6ee20d42373fbb06dcf44b3c95849
beliyandr <beliyandr@gmail.com>
2024-08-11 19:58:05 +0300

    123

---
+++ React_TS_Nepomn/src/components/App.tsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect, useRef } from "react";
+import { ITodo } from "../types/data";
+
+export const App: React.FC = () => {
+  const [value, setValue] = useState("");
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const addTodo = () => {
+    setTodos([
+      ...todos,
+      {
+        id: Date.now(),
+        title: value,
+        complete: false,
+      },
+    ]);
+  };
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button onClick={addTodo}>Add</button>
+      </div>
+    </div>
+  );
+};
