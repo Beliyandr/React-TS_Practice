300852fc425782b18b694a1f708981fc96120fb1
beliyandr <beliyandr@gmail.com>
2024-08-11 23:25:13 +0300

    1321

--- React_TS_Nepomn/src/components/App.tsx
+++ React_TS_Nepomn/src/components/App.tsx
@@ -1,19 +1,55 @@
 import React, { useState, useEffect, useRef } from "react";
 import { ITodo } from "../types/data";
+import { TodoList } from "./TodoList";
 
 export const App: React.FC = () => {
   const [value, setValue] = useState("");
   const [todos, setTodos] = useState<ITodo[]>([]);
 
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setValue(e.target.value);
+  };
   const addTodo = () => {
-    setTodos([
-      ...todos,
-      {
-        id: Date.now(),
-        title: value,
-        complete: false,
-      },
-    ]);
+    if (value) {
+      setTodos([
+        ...todos,
+        {
+          id: Date.now(),
+          title: value,
+          complete: false,
+        },
+      ]);
+
+      setValue("");
+    }
+  };
+
+  const handleKeyDown: React.KeyboardEventHandler<HTMLInputElement> = (e) => {
+    if (e.key === "Enter") {
+      addTodo();
+    }
+  };
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
+
+  const removeTodo = (id: number): void => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+  const toggleTodo = (id: number): void => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id !== id) return todo;
+
+        return {
+          ...todo,
+          complete: !todo.complete,
+        };
+      })
+    );
   };
 
   return (
@@ -22,10 +58,13 @@ export const App: React.FC = () => {
         <input
           type="text"
           value={value}
-          onChange={(e) => setValue(e.target.value)}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+          ref={inputRef}
         />
         <button onClick={addTodo}>Add</button>
       </div>
+      <TodoList items={todos} removeTodo={removeTodo} toggleTodo={toggleTodo} />
     </div>
   );
 };
