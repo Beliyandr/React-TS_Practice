79dd1d9efe073188e06b15a04b8aac433ae3382e
beliyandr <beliyandr@gmail.com>
2024-08-11 16:34:43 +0300

    123

--- UILBI_REACT/RTS/src/components/EventsExample.tsx
+++ UILBI_REACT/RTS/src/components/EventsExample.tsx
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 export const EventsExample: React.FC = () => {
   const [value, setValue] = useState<string>("");
+  const [isDrag, setIsDrag] = useState(false);
+
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
@@ -9,24 +12,39 @@ export const EventsExample: React.FC = () => {
 
   const clickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
     console.log(value);
+    console.log(inputRef.current?.value);
   };
 
   const dragHandler = (e: React.DragEvent<HTMLDivElement>) => {
     console.log("Drag");
   };
 
-  function leaveHandler(event: React.DragEvent<HTMLDivElement>): void {
-    throw new Error("Function not implemented.");
-  }
-
   const dragWithPreventHandler = (event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
+    setIsDrag(true);
   };
-  const dropHandler = () => {};
+
+  const leaveHandler = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setIsDrag(false);
+  };
+
+  const dropHandler = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setIsDrag(false);
+
+    console.log("DROP");
+  };
 
   return (
     <div>
-      <input type="text" value={value} onChange={changeHandler} />
+      <input
+        type="text"
+        value={value}
+        onChange={changeHandler}
+        placeholder="управляемый"
+      />
+      <input type="text" ref={inputRef} placeholder="Неуправляемый" />
       <button onClick={clickHandler}>Click</button>
 
       <div
@@ -46,7 +64,7 @@ export const EventsExample: React.FC = () => {
         style={{
           width: "200px",
           height: "200px",
-          background: "green",
+          background: isDrag ? "blue" : "green",
           margin: "10px 5px",
         }}
       ></div>
---
+++ UILBI_REACT/Redux/my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div style={{ display: 'flex' }}>
+        <button> Пополнить счет</button>
+        <button> Снять со счета</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
