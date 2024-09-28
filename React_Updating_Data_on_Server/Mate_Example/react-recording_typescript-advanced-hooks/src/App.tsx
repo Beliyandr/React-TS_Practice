63d3ab5f1ad7df0db891591823002177c93e5d52
Andrew <beliyandr@gmail.com>
2024-09-28 22:16:10 +0300

    useRef

--- React_Updating_Data_on_Server/Mate_Example/react-recording_typescript-advanced-hooks/src/App.tsx
+++ React_Updating_Data_on_Server/Mate_Example/react-recording_typescript-advanced-hooks/src/App.tsx
@@ -1,35 +1,77 @@
 // #region imports
-import React, { useState } from 'react';
+import React, { useCallback, useMemo, useRef, useState } from "react";
 
-import { Post } from './types/Post';
-import { getMaxId, getPreparedPosts } from './services/posts';
-import { PostForm } from './components/PostForm';
-import { PostList } from './components/PostList';
+import { Post } from "./types/Post";
+import { getMaxId, getPreparedPosts } from "./services/posts";
+import { PostForm } from "./components/PostForm";
+import { PostList } from "./components/PostList";
 // #endregion
 
 export const App: React.FC = () => {
   // #region query
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
+
+  const [count, setCount] = useState(0);
+
+  const [appliedQuery, setAppliedQuery] = useState("");
+
+  const timerId = useRef(0);
 
   const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(event.target.value);
-  }
+
+    // window.clearTimeout();
+
+    timerId.current = window.setTimeout(() => {
+      console.log("filterring by", event.target.value);
+      setAppliedQuery(event.target.value);
+    }, 1000);
+  };
   // #endregion
   // #region posts
   const [posts, setPosts] = useState<Post[]>(getPreparedPosts());
+  const [selectedPost, setSelectedPost] = useState<Post | null>(null);
 
-  const addPost = (post: Post) => {
-    const newPost = {
-      ...post,
-      id: getMaxId(posts) + 1,
-    };
+  const addPost = useCallback((post: Post) => {
+    setPosts((currentPosts) => {
+      const newPost = {
+        ...post,
+        id: getMaxId(posts) + 1,
+      };
+      return [newPost, ...currentPosts];
+    });
+  }, []);
 
-    setPosts(currentPosts => [newPost, ...currentPosts]);
-  };
+  const deletePost = useCallback((postId: number) => {
+    setPosts((currentPosts) =>
+      currentPosts.filter((post) => post.id !== postId)
+    );
+  }, []);
+
+  const updatePost = useCallback((updatedPost: Post) => {
+    setPosts((currentPosts) => {
+      const newPosts = [...currentPosts];
+      const index = newPosts.findIndex((post) => post.id === updatedPost.id);
+
+      newPosts.splice(index, 1, updatedPost);
+
+      return newPosts;
+    });
+  }, []);
+
   // #endregion
+
+  const filteredPosts = useMemo(() => {
+    return posts.filter((post) => post.title.includes(query));
+  }, [posts, appliedQuery]);
+
+  console.log(selectedPost);
 
   return (
     <div className="section py-5">
+      <button className="button" onClick={() => setCount((count) => count + 1)}>
+        {count}
+      </button>
       <div className="columns is-mobile">
         <div className="column">
           <h1 className="title">Posts</h1>
@@ -45,8 +87,23 @@ export const App: React.FC = () => {
         </div>
       </div>
 
-      <PostList posts={posts} />
-      <PostForm onSubmit={addPost} />
-      </div>
+      <PostList
+        posts={filteredPosts}
+        selectedPostId={selectedPost?.id}
+        onDelete={deletePost}
+        onSelect={setSelectedPost}
+      />
+
+      {selectedPost ? (
+        <PostForm
+          onSubmit={updatePost}
+          post={selectedPost}
+          key={selectedPost.id}
+          onReset={() => setSelectedPost(null)}
+        />
+      ) : (
+        <PostForm onSubmit={addPost} />
+      )}
+    </div>
   );
 };
--- React_Updating_Data_on_Server/Mate_Example/react-recording_typescript-advanced-hooks/src/components/PostForm.tsx
+++ React_Updating_Data_on_Server/Mate_Example/react-recording_typescript-advanced-hooks/src/components/PostForm.tsx
@@ -1,189 +1,213 @@
 // #region imports
-import classNames from 'classnames';
-import React, { useState } from 'react';
-import { Post } from '../types/Post';
-import { getAllUsers, getUserById } from '../services/user';
+import classNames from "classnames";
+import React, { useEffect, useRef, useState } from "react";
+import { Post } from "../types/Post";
+import { getAllUsers, getUserById } from "../services/user";
 
 type Props = {
   onSubmit: (post: Post) => void;
+  onReset?: () => void;
+  post?: Post;
 };
 // #endregion
 
-export const PostForm: React.FC<Props> = ({ onSubmit }) => {
-  // #region state
-  const [title, setTitle] = useState('');
-  const [hasTitleError, setHasTitleError] = useState(false);
+export const PostForm: React.FC<Props> = React.memo(
+  ({ onSubmit, post, onReset = () => {} }) => {
+    console.log("render PostForm");
 
-  const [userId, setUserId] = useState(0);
-  const [hasUserIdError, setHasUserIdError] = useState(false);
-  
-  const [body, setBody] = useState('');
-  const [bodyErrorMessage, setBodyErrorMessage] = useState('');
-  // #endregion
-  // #region handlers
-  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTitle(event.target.value);
-    setHasTitleError(false);
-  };
+    const titleField = useRef<HTMLInputElement | null>(null);
+    console.log(titleField.current);
 
-  const handleUserIdChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setUserId(+event.target.value);
-    setHasUserIdError(false);
-  };
+    useEffect(() => {
+      if (titleField.current && post) {
+        titleField.current.focus();
+      }
+    }, []);
 
-  const handleBodyChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setBody(event.target.value);
-    setBodyErrorMessage('');
-  };
-  // #endregion
-  // #region submit
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
+    // #region state
+    const [title, setTitle] = useState(post?.title || "");
+    const [hasTitleError, setHasTitleError] = useState(false);
 
-    setHasTitleError(!title);
-    setHasUserIdError(!userId);
+    const [userId, setUserId] = useState(post?.userId || 0);
+    const [hasUserIdError, setHasUserIdError] = useState(false);
 
-    if (!body) {
-      setBodyErrorMessage('Please enter some text');
-    } else if (body.length < 5) {
-      setBodyErrorMessage('Body should have at least 5 chars');
-    }
+    const [body, setBody] = useState(post?.body || "");
+    const [bodyErrorMessage, setBodyErrorMessage] = useState("");
+    // #endregion
+    // #region handlers
+    const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setTitle(event.target.value);
+      setHasTitleError(false);
+    };
 
-    if (!title || !userId || body.length < 5) {
-      return;
-    }
+    const handleUserIdChange = (
+      event: React.ChangeEvent<HTMLSelectElement>
+    ) => {
+      setUserId(+event.target.value);
+      setHasUserIdError(false);
+    };
 
-    onSubmit({
-      id: 0,
-      title,
-      body,
-      userId,
-      user: getUserById(userId),
-    });
+    const handleBodyChange = (
+      event: React.ChangeEvent<HTMLTextAreaElement>
+    ) => {
+      setBody(event.target.value);
+      setBodyErrorMessage("");
+    };
+    // #endregion
+    // #region submit
+    const handleSubmit = (event: React.FormEvent) => {
+      event.preventDefault();
 
-    reset();
-  };
-  // #endregion
-  // #region reset
-  const reset = () => {
-    setTitle('');
-    setUserId(0);
-    setBody('');
+      setHasTitleError(!title);
+      setHasUserIdError(!userId);
 
-    setHasTitleError(false);
-    setHasUserIdError(false);
-    setBodyErrorMessage('');
-  };
-  // #endregion
+      if (!body) {
+        setBodyErrorMessage("Please enter some text");
+      } else if (body.length < 5) {
+        setBodyErrorMessage("Body should have at least 5 chars");
+      }
 
-  return (
-    <form 
-      action="/api/posts" 
-      method="POST" 
-      className="box"
-      onSubmit={handleSubmit}
-      onReset={reset}
-    >
-      <h2 className="title is-4">Create a post</h2>
+      if (!title || !userId || body.length < 5) {
+        return;
+      }
 
-      <div className="field">
-        <label className="label" htmlFor="post-title">
-          Title
-        </label>
+      onSubmit({
+        id: post?.id || 0,
+        title,
+        body,
+        userId,
+        user: getUserById(userId),
+      });
 
-        <div className={classNames('control', {
-          'has-icons-right': hasTitleError,
-        })}>
-          <input
-            id="post-title"
-            className={classNames('input', {
-              'is-danger': hasTitleError
-            })} 
-            type="text" 
-            placeholder="Email input" 
-            value={title}
-            onChange={handleTitleChange}
-            onBlur={() => {
-              setHasTitleError(!title);
-            }}
-          />
+      reset();
+    };
+    // #endregion
+    // #region reset
+    const reset = () => {
+      setTitle("");
+      setUserId(0);
+      setBody("");
+
+      setHasTitleError(false);
+      setHasUserIdError(false);
+      setBodyErrorMessage("");
+
+      onReset();
+    };
+    // #endregion
+
+    return (
+      <form
+        action="/api/posts"
+        method="POST"
+        className="box"
+        onSubmit={handleSubmit}
+        onReset={reset}
+      >
+        <h2 className="title is-4">Create a post</h2>
+
+        <div className="field">
+          <label className="label" htmlFor="post-title">
+            Title
+          </label>
+
+          <div
+            className={classNames("control", {
+              "has-icons-right": hasTitleError,
+            })}
+          >
+            <input
+              id="post-title"
+              ref={titleField}
+              className={classNames("input", {
+                "is-danger": hasTitleError,
+              })}
+              type="text"
+              placeholder="Email input"
+              value={title}
+              onChange={handleTitleChange}
+              onBlur={() => {
+                setHasTitleError(!title);
+              }}
+            />
+
+            {hasTitleError && (
+              <span className="icon is-small is-right">
+                <i className="fas fa-exclamation-triangle has-text-danger"></i>
+              </span>
+            )}
+          </div>
 
           {hasTitleError && (
-            <span className="icon is-small is-right">
-              <i className="fas fa-exclamation-triangle has-text-danger"></i>
-            </span>
+            <p className="help is-danger">Please enter a title</p>
           )}
         </div>
 
-        {hasTitleError && (
-          <p className="help is-danger">Please enter a title</p>
-        )}
-      </div>
+        <div className="field">
+          <label className="label" htmlFor="post-user-id">
+            Subject
+          </label>
 
-      <div className="field">
-        <label className="label" htmlFor="post-user-id">
-          Subject
-        </label>
-
-        <div className="control has-icons-left">
-          <div className={classNames('select', {
-            'is-danger': hasUserIdError,
-          })}>
-            <select
-              id="post-user-id"
-              value={userId}
-              onChange={handleUserIdChange}
+          <div className="control has-icons-left">
+            <div
+              className={classNames("select", {
+                "is-danger": hasUserIdError,
+              })}
             >
-              <option value="0">Select a user</option>
+              <select
+                id="post-user-id"
+                value={userId}
+                onChange={handleUserIdChange}
+              >
+                <option value="0">Select a user</option>
 
-              {getAllUsers().map(user => (
-                <option value={user.id} key={user.id}>
-                  {user.name}
-                </option>
-              ))}
-            </select>
+                {getAllUsers().map((user) => (
+                  <option value={user.id} key={user.id}>
+                    {user.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            <span className="icon is-small is-left">
+              <i className="fas fa-user"></i>
+            </span>
           </div>
 
-          <span className="icon is-small is-left">
-            <i className="fas fa-user"></i>
-          </span>
+          {hasUserIdError && (
+            <p className="help is-danger">Please select a user</p>
+          )}
         </div>
 
-        {hasUserIdError && (
-          <p className="help is-danger">Please select a user</p>
-        )}
-      </div>
+        <div className="field">
+          <label className="label">Message</label>
 
-      <div className="field">
-        <label className="label">
-          Message
-        </label>
+          <div className="control">
+            <textarea
+              className={classNames("textarea", {
+                "is-danger": bodyErrorMessage,
+              })}
+              placeholder="Add some text here"
+              value={body}
+              onChange={handleBodyChange}
+            ></textarea>
+          </div>
 
-        <div className="control">
-          <textarea 
-            className={classNames('textarea', {
-              'is-danger': bodyErrorMessage,
-            })} 
-            placeholder="Add some text here"
-            value={body}
-            onChange={handleBodyChange}
-          ></textarea>
+          {bodyErrorMessage && (
+            <p className="help is-danger">{bodyErrorMessage}</p>
+          )}
         </div>
 
-        {bodyErrorMessage && (
-          <p className="help is-danger">{bodyErrorMessage}</p>
-        )}
-      </div>
+        <div className="buttons">
+          <button type="submit" className="button is-link">
+            Submit
+          </button>
 
-      <div className="buttons">
-        <button type="submit" className="button is-link">
-          Submit
-        </button>
-
-        <button type="reset" className="button is-link is-light">
-          Cancel
-        </button>
-      </div>
-    </form>
-  );
-};
+          <button type="reset" className="button is-link is-light">
+            Cancel
+          </button>
+        </div>
+      </form>
+    );
+  }
+);
