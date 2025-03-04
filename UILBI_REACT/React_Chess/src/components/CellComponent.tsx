81e0782d7a4622117a00410aca7c2da9680232df
beliyandr <beliyandr@gmail.com>
2025-03-04 16:30:37 +0200

    543

--- UILBI_REACT/React_Chess/src/components/CellComponent.tsx
+++ UILBI_REACT/React_Chess/src/components/CellComponent.tsx
@@ -3,8 +3,19 @@ import { Cell } from "../models/Cell";
 
 interface Props {
   cell: Cell;
+  selected: boolean;
+  click: (cell: Cell) => void;
 }
 
-export const CellComponent: FC<Props> = ({ cell }) => {
-  return <div className={["cell", cell.color].join(" ")}>{cell.figure}</div>;
+export const CellComponent: FC<Props> = ({ cell, selected, click }) => {
+  return (
+    <div
+      className={["cell", cell.color, selected ? "selected" : ""].join(" ")}
+      onClick={() => click(cell)}
+      style={{ background: cell.available && cell.figure ? "green" : "" }}
+    >
+      {cell.available && !cell.figure && <div className={"available"} />}
+      {cell.figure?.logo && <img src={cell.figure.logo} alt="queen" />}
+    </div>
+  );
 };
