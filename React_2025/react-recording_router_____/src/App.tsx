1ead0c6984f8fbde9af497449fee058c288cc2c1
beliyandr <beliyandr@gmail.com>
2025-03-27 16:32:21 +0200

    423

---
+++ React_2025/react-recording_router_____/src/App.tsx
@@ -0,0 +1,47 @@
+import { Routes, Route, Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { NewPostPage } from './pages/NewPostPage';
+import { PostDetailsPage } from './pages/PostDetailsPage';
+import { PostsPage } from './pages/PostsPage';
+import { UsersPage } from './pages/UsersPage';
+import { HomePage } from './pages/HomePage';
+
+
+type Page = 'home' | 'users' | 'posts' | 'postDetails' | 'newPost';
+
+export const App: React.FC = () => {
+
+  return <>
+    <nav className="navbar is-light px-3">
+      <div className="navbar-brand">
+        <Link to="/" className="navbar-item">
+          <img src="/logo.svg" alt="MA" className="logo" />
+        </Link>
+
+        <Link to="/" className="navbar-item is-active" >
+          Home
+        </Link>
+
+        <Link to="/users" className="navbar-item" >
+          Users
+        </Link>
+
+        <Link to="/posts" className="navbar-item" >
+          Posts
+        </Link>
+      </div>
+    </nav>
+
+    <div className="section">
+
+      <Routes >
+        <Route path='/' element={<HomePage />} />
+        <Route path='/users' element={<UsersPage />} />
+        <Route path='/posts' element={<PostsPage />} />
+        <Route path='/posts/620' element={<PostDetailsPage />} />
+        <Route path='/posts/new' element={<NewPostPage />} />
+      </Routes>
+
+    </div>
+  </>;
+}
---
+++ React_2025/react-recording_router_____/src/index.tsx
@@ -0,0 +1,25 @@
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'bulma';
+import './index.scss';
+import { App } from './App';
+import { PostsProvider } from './store/PostsContext';
+import { UsersProvider } from './store/UsersContext';
+
+const container = document.getElementById('root') as HTMLElement;
+
+
+const Root = () => (
+
+  <Router>
+    <UsersProvider>
+      <PostsProvider>
+        <App />
+      </PostsProvider>
+    </UsersProvider>
+  </Router>
+)
+
+
+createRoot(container).render(<Root />);
