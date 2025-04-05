24863ee5c270d3bc6e21c83b9fd067c01d5a9e5c
beliyandr <beliyandr@gmail.com>
2025-04-05 22:52:43 +0300

    4

--- React_2025/react-recording_router_____/src/Root.tsx
+++ React_2025/react-recording_router_____/src/Root.tsx
@@ -9,35 +9,38 @@ import { HomePage } from "./pages/HomePage";
 import { App } from "./App";
 import { LoginPage } from "./pages/LoginPage";
 import { RequireAuth } from "./pages/RequireAuth";
+import { AuthProvider } from "./store/AuthContext";
 
 export const Root = () => (
   <Router>
-    <UsersProvider>
-      <PostsProvider>
-        <Routes>
-          <Route path="/" element={<App />}>
-            <Route index element={<HomePage />} />
-            <Route path='login' element={<LoginPage />} />
+    <AuthProvider>
+      <UsersProvider>
+        <PostsProvider>
+          <Routes>
+            <Route path="/" element={<App />}>
+              <Route index element={<HomePage />} />
+              <Route path="login" element={<LoginPage />} />
 
-            <Route path="users" element={<RequireAuth />}>
-              <Route index element={<UsersPage />} />
+              <Route path="users" element={<RequireAuth />}>
+                <Route index element={<UsersPage />} />
 
-              <Route path=":userId?/posts">
+                <Route path=":userId?/posts">
+                  <Route index element={<PostsPage />} />
+                  <Route path=":postId" element={<PostDetailsPage />} />
+                  <Route path="new" element={<NewPostPage />} />
+                </Route>
+              </Route>
+
+              <Route path="posts">
                 <Route index element={<PostsPage />} />
                 <Route path=":postId" element={<PostDetailsPage />} />
                 <Route path="new" element={<NewPostPage />} />
               </Route>
+              <Route path="*" element={<p>404 Not Found</p>} />
             </Route>
-
-            <Route path="posts">
-              <Route index element={<PostsPage />} />
-              <Route path=":postId" element={<PostDetailsPage />} />
-              <Route path="new" element={<NewPostPage />} />
-            </Route>
-            <Route path="*" element={<p>404 Not Found</p>} />
-          </Route>
-        </Routes>
-      </PostsProvider>
-    </UsersProvider>
+          </Routes>
+        </PostsProvider>
+      </UsersProvider>
+    </AuthProvider>
   </Router>
 );
