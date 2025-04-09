1ea9eb85ebd46dc7309557e76dd8e21372136638
beliyandr <beliyandr@gmail.com>
2025-04-09 16:46:53 +0300

    432

--- React_TS_Nepomn/routing/src/App.tsx
+++ React_TS_Nepomn/routing/src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { Routes, Route, Link, Navigate } from "react-router-dom";
+import {
+  Route,
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import { Aboutpage } from "./pages/Aboutpage";
 import { HomePage } from "./pages/HomePage";
-import { Blogpage } from "./pages/Blogpage";
+import { blogLoader, Blogpage } from "./pages/Blogpage";
 import { Notfoundpage } from "./pages/Notfoundpage";
-import { SinglePage } from "./pages/SinglePage";
+import { postLoader, SinglePage } from "./pages/SinglePage";
 import { CreatePost } from "./pages/CreatePost";
 import { EditPost } from "./pages/EditPost";
 import { LoginPage } from "./pages/LoginPage";
@@ -14,32 +20,36 @@ import { Layout } from "./components/Layout";
 import { RequireAuth } from "./hoc/RequireAuth";
 import { AuthProvider } from "./hoc/AuthProvider";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<HomePage />} />
+      <Route path="about" element={<Aboutpage />}>
+        <Route path="contacts" element={<p>Our contacts</p>} />
+        <Route path="team" element={<p>Our Team</p>} />
+      </Route>
+      <Route path="about-us" element={<Navigate to="/about" replace />} />
+      <Route path="posts" element={<Blogpage />} loader={blogLoader} />
+      <Route path="posts/:id" element={<SinglePage />} loader={postLoader} />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="posts/:id/edit" element={<EditPost />} />
+      <Route
+        path="posts/new"
+        element={
+          <RequireAuth>
+            <CreatePost />
+          </RequireAuth>
+        }
+      />
+      <Route path="*" element={<Notfoundpage />} />
+    </Route>
+  )
+);
+
 const App: React.FC = () => {
   return (
     <AuthProvider>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="about" element={<Aboutpage />}>
-            <Route path="contacts" element={<p>Our contacts</p>} />
-            <Route path="team" element={<p>Our Team</p>} />
-          </Route>
-          <Route path="about-us" element={<Navigate to="/about" replace />} />
-          <Route path="posts" element={<Blogpage />} />
-          <Route path="posts/:id" element={<SinglePage />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="posts/:id/edit" element={<EditPost />} />
-          <Route
-            path="posts/new"
-            element={
-              <RequireAuth>
-                <CreatePost />
-              </RequireAuth>
-            }
-          />
-          <Route path="*" element={<Notfoundpage />} />
-        </Route>
-      </Routes>
+      <RouterProvider router={router} />
     </AuthProvider>
   );
 };
